3e794cc1a62a02ac3ff79cc0ce1c0c2153dcf11b
Antti Nupponen <nupponen.antti@aalto.fi>
2021-12-21 19:32:50 +0200

    v2.6

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -6,19 +6,49 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const Person = ({person}) => (
+    <li>
+      {person.name}
+    </li>
+  )
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    console.log("addPerson event info: ", event)
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person, idx) => <Person key={idx} person={person}/>)}
+      </ul>
     </div>
   )
 
