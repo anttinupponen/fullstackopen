2c895e3da2d18002b69ce455092ae49f9ade66be
Antti Nupponen <nupponen.antti@gmail.com>
2024-11-30 15:07:43 +0200

    fixed 6.19 middleware typo in action type

--- part6/anecdotes_redux/src/reducers/filterReducer.jsx
+++ part6/anecdotes_redux/src/reducers/filterReducer.jsx
@@ -1,21 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// export const setFilter = filter => {
-//   return {
-//     type: 'SET_FILTER',
-//     filter
-//   }
-// }
-
-// const reducer = (state = '', action) => {
-//   switch (action.type) {
-//     case 'SET_FILTER':
-//       return action.filter
-//     default:
-//       return state
-//   }
-// }
-
 const filterSlice = createSlice({
   name: 'filter',
   initialState: '',
--- part6/anecdotes_redux/src/store.js
+++ part6/anecdotes_redux/src/store.js
@@ -5,9 +5,10 @@ import notificationReducer from './reducers/notificationReducer'
 import { setTimedNotification } from './reducers/notificationReducer'
 
 const notificationMiddleware = store => next => action => {
+  console.log('notificationMiddleware', action)
   if (action.type === 'anecdotes/appendAnecdote') {
     store.dispatch(setTimedNotification(`You created ${action.payload.content}`, 5))
-  } else if (action.type === 'anecdotes/voteAnecdote') {
+  } else if (action.type === 'anecdotes/updateAnecdoteLikes') {
     store.dispatch(setTimedNotification(`You voted for ${action.payload.content}`, 5))
   }
   return next(action)
